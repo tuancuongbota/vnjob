e3be16b42be81facfaa1fb9fd8d497ed60098a7a
tuancuongbota <tuancuong.bota@gmail.com>
2021-11-15 17:08:57 +0700

    cuongnt

--- js/js_custom.js
+++ js/js_custom.js
@@ -151,6 +151,41 @@ $(document).ready(function() {
     }
     ]
   });
+  $('.slick_recruit_relate').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rows:3,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+  });
   $('.bota_career_slide').slick({
     dots: false,
     infinite: true,
